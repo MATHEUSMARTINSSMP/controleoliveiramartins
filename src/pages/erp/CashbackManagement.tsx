c33c20c1d1a12fcf60e3b11dc63fca4a6b62ac32
Matheus Martins <matheusmartinss@icloud.com>
2025-11-30 02:21:43 -0300

    fix: Adicionar chamada de fetchCategoriasProdutos no useEffect

--- src/pages/erp/CashbackManagement.tsx
+++ src/pages/erp/CashbackManagement.tsx
@@ -330,6 +330,7 @@ export default function CashbackManagement() {
   useEffect(() => {
     if (!authLoading && profile) {
       fetchData();
+      fetchCategoriasProdutos();
     }
   }, [authLoading, profile]);
 
